855d6ff65fdea8263f820f99825789fc407ae8df
Houke de Kwant <houke@ivaldi.nl>
2017-02-15 20:54:54 +0100

    rename to picker

--- assets/js/input.js
+++ assets/js/input.js
@@ -4,18 +4,18 @@
 	jQuery(document).on('click', 'li[data-svg]', function(){
 		var val = jQuery(this).data('svg');
 		active_item.find('input').val(val);
-		active_item.find('.iv-icon-selector__svg').html('<img src="'+ jQuery(this).find('img').attr('src') + '" alt=""/>');
-		jQuery('.iv-icon-selector__popup-holder').remove();
+		active_item.find('.acf-icon-picker__svg').html('<img src="'+ jQuery(this).find('img').attr('src') + '" alt=""/>');
+		jQuery('.acf-icon-picker__popup-holder').remove();
 	});
-	jQuery(document).on('click', '.iv-icon-selector__popup-holder', function(e){
+	jQuery(document).on('click', '.acf-icon-picker__popup-holder', function(e){
 		e.preventDefault();
 		e.stopPropagation();
 		active_item = "";
-		jQuery('.iv-icon-selector__popup-holder').remove();
+		jQuery('.acf-icon-picker__popup-holder').remove();
 	});
 
 	function initialize_field( $el ) {
-		$el.find('.iv-icon-selector__img').on('click', function(e){
+		$el.find('.acf-icon-picker__img').on('click', function(e){
 			e.preventDefault();
 			active_item = $(this);
 			if(iv.svgs.length == 0){
@@ -25,7 +25,7 @@
 				jQuery(iv.svgs).each(function(){
 					var svg = jQuery(this)[0];
 					list += `<li data-svg="${svg['name']}">
-						<div class="iv-icon-selector__popup-svg">
+						<div class="acf-icon-picker__popup-svg">
 							<img src="${iv.path}${svg['icon']}" alt=""/>
 						</div>
 						<span>${svg['name'].replace(/[-_]/g, ' ')}</span>
@@ -34,8 +34,8 @@
 				list += `</ul>`;
 			}
 
-			jQuery('body').append(`<div class="iv-icon-selector__popup-holder">
-				<div class="iv-icon-selector__popup">
+			jQuery('body').append(`<div class="acf-icon-picker__popup-holder">
+				<div class="acf-icon-picker__popup">
 					${list}
 				</div>
 			</div>`);
@@ -44,7 +44,7 @@
 
 	if( typeof acf.add_action !== 'undefined' ) {
 		acf.add_action('ready append', function( $el ){
-			acf.get_fields({ type : 'icon_selector'}, $el).each(function(){
+			acf.get_fields({ type : 'icon_picker'}, $el).each(function(){
 				initialize_field( $(this) );
 			});
 		});
--- fields/acf-icon-picker-v5.php
+++ fields/acf-icon-picker-v5.php
@@ -2,15 +2,15 @@
 
 if( ! defined( 'ABSPATH' ) ) exit;
 
-if( !class_exists('acf_field_icon_selector') ) :
+if( !class_exists('acf_plugin_icon_picker') ) :
 
-class acf_field_icon_selector extends acf_field {
+class acf_plugin_icon_picker extends acf_field {
 
 	function __construct( $settings ) {
 
-		$this->name = 'icon_selector';
+		$this->name = 'icon-picker';
 
-		$this->label = __('Icon Picker', 'acf-icon_selector');
+		$this->label = __('Icon Picker', 'acf-icon-picker');
 
 		$this->category = 'jquery';
 
@@ -19,7 +19,7 @@ class acf_field_icon_selector extends acf_field {
 		);
 
 		$this->l10n = array(
-			'error'	=> __('Error!', 'acf-icon_selector'),
+			'error'	=> __('Error!', 'acf-icon-picker'),
 		);
 
 		$this->settings = $settings;
@@ -58,17 +58,17 @@ class acf_field_icon_selector extends acf_field {
 		$input_icon = $field['value'] != "" ? $field['value'] : $field['initial_value'];
 		$svg = $this->path . $input_icon . '.svg';
 		?>
-			<div class="iv-icon-selector">
-				<div class="iv-icon-selector__img">
+			<div class="acf-icon-picker">
+				<div class="acf-icon-picker__img">
 					<?php
 						if ( file_exists( $svg ) ) {
 							$svg = $this->url . $input_icon . '.svg';
-							echo '<div class="iv-icon-selector__svg">';
+							echo '<div class="acf-icon-picker__svg">';
 						   	echo '<img src="'.$svg.'" alt=""/>';
 						    echo '</div>';
 						}else{
-							echo '<div class="iv-icon-selector__svg">';
-							echo '<span class="iv-icon-selector__svg--span">&plus;</span>';
+							echo '<div class="acf-icon-picker__svg">';
+							echo '<span class="acf-icon-picker__svg--span">&plus;</span>';
 						    echo '</div>';
 						}
 					?>
@@ -83,21 +83,21 @@ class acf_field_icon_selector extends acf_field {
 		$url = $this->settings['url'];
 		$version = $this->settings['version'];
 
-		wp_register_script( 'acf-input-icon_selector', "{$url}assets/js/input.js", array('acf-input'), $version );
-		wp_enqueue_script('acf-input-icon_selector');
+		wp_register_script( 'acf-input-icon-picker', "{$url}assets/js/input.js", array('acf-input'), $version );
+		wp_enqueue_script('acf-input-icon-picker');
 
-		wp_localize_script( 'acf-input-icon_selector', 'iv', array(
+		wp_localize_script( 'acf-input-icon-picker', 'iv', array(
 			'path' => $this->url,
 			'svgs' => $this->svgs,
-			'no_icons_msg' => sprintf( esc_html__('To add icons, add your svg files in the /%s folder in your theme.', 'acf-icon_selector'), $this->path_suffix)
+			'no_icons_msg' => sprintf( esc_html__('To add icons, add your svg files in the /%s folder in your theme.', 'acf-icon-picker'), $this->path_suffix)
 		) );
 
-		wp_register_style( 'acf-input-icon_selector', "{$url}assets/css/input.css", array('acf-input'), $version );
-		wp_enqueue_style('acf-input-icon_selector');
+		wp_register_style( 'acf-input-icon-picker', "{$url}assets/css/input.css", array('acf-input'), $version );
+		wp_enqueue_style('acf-input-icon-picker');
 	}
 }
 
-new acf_field_icon_selector( $this->settings );
+new acf_plugin_icon_picker( $this->settings );
 
 endif;
 
