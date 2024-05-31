e2f322b2fe8893eaf7fc0f534df8da5d0d76db94
Erik van der Bas <erik@basedonline.nl>
2024-05-31 22:19:30 +0200

    feat: acf6.3 compat

--- acf-icon-picker.php
+++ acf-icon-picker.php
@@ -10,34 +10,37 @@ License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 GitHub Plugin URI: https://github.com/houke/acf-icon-picker
 GitHub Branch: master
-*/
+ */
 
-if( ! defined( 'ABSPATH' ) ) exit;
-
-if( !class_exists('acf_plugin_icon_picker') ) :
-
-class acf_plugin_icon_picker {
-
-	public $settings = array();
-	
-	function __construct() {
-
-		$this->settings = array(
-			'version'	=> '1.9.1',
-			'url'		=> plugin_dir_url( __FILE__ ),
-			'path'		=> plugin_dir_path( __FILE__ )
-		);
-
-		add_action('acf/include_field_types', 	array($this, 'include_field_types'));
-
-	}
-
-	function include_field_types( $version = false ) {
-		include_once('fields/acf-icon-picker-v5.php');
-	}
-
+if (!defined('ABSPATH')) {
+	exit;
 }
 
-new acf_plugin_icon_picker();
+if (!class_exists('acf_plugin_icon_picker')) :
+
+	class acf_plugin_icon_picker
+	{
+
+		public $settings = array();
+
+		public function __construct()
+		{
+
+			$this->settings = array(
+				'version' => '1.9.1',
+				'url'     => plugin_dir_url(__FILE__),
+				'path'    => plugin_dir_path(__FILE__),
+			);
+
+			add_action('acf/include_field_types', array($this, 'include_field_types'));
+		}
+
+		public function include_field_types($version = false)
+		{
+			include_once 'fields/acf-icon-picker-v5.php';
+		}
+	}
+
+	new acf_plugin_icon_picker();
 
 endif;
